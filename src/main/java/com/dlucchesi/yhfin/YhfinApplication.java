5662d01f3155c4b564fe1d36309ca26b075b2282
dlucchesi <dlucchesi@gmail.com>
2023-03-26 21:55:11 -0300

    Init

---
+++ src/main/java/com/dlucchesi/yhfin/YhfinApplication.java
@@ -0,0 +1,13 @@
+package com.dlucchesi.yhfin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YhfinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YhfinApplication.class, args);
+	}
+
+}
